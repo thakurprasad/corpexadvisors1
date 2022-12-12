3c18e43175c8feab534e3553de9012569c7de78a
Thakur Prasad <thakurprasadlhs@gmail.com>
2022-12-12 17:46:17 +0530

    display required_documents on service page

--- resources/views/startup-registration/service.blade.php
+++ resources/views/startup-registration/service.blade.php
@@ -24,6 +24,21 @@
  		<div class="row" style="min-height:600px;"> 
  			<h1>{!! $service->name !!}</h1>
  			{!! $service !!}
+ 			<div class="col-md-12 row">
+ 				<h4>Documents Required </h4>
+<p>Only Scanned Copies are needed, Scanned copy of PAN Card of all directors and Aadhar card/ Voter ID/ Passport/ Driving</p>
+<?php $data = json_decode($service, true); 
+$required_documents = json_decode($data['required_documents'], true);
+
+?>
+		@foreach($required_documents as $key=>$document)
+			<div class="col-md-3">
+				<h6> {!! $key !!}</h6>
+				<p>{!! $document !!}</p>
+			</div>
+		@endforeach
+ 			</div>
+
  		</div>	
  	</div>
 <style type="text/css">	
