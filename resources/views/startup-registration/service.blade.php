967a6a47975d8fb08d76235bbd0cf4ef46e41fa9
Thakur Prasad <thakurprasadlhs@gmail.com>
2022-12-13 19:43:00 +0530

    add working process

--- resources/views/startup-registration/service.blade.php
+++ resources/views/startup-registration/service.blade.php
@@ -20,51 +20,73 @@
 @endsection
 
 @section('body')
- 	<div class="contener">
- 		<div class="row"> 
- 			<h1>{!! $service->name !!}</h1>
- 			{!! $service !!}
- 			<div class="col-md-12 row">
- 				<h4>Documents Required </h4>
-<p>Only Scanned Copies are needed, Scanned copy of PAN Card of all directors and Aadhar card/ Voter ID/ Passport/ Driving</p>
-<?php $data = json_decode($service, true); 
-$required_documents = json_decode($data['required_documents'], true);
-$working_process = json_decode($data['working_process'], true);
-
+<?php 
+	$data = json_decode($service, true); 
+	$required_documents = json_decode($data['required_documents'], true);
+	$working_process = json_decode($data['working_process'], true);
 ?>
-		@foreach($required_documents as $key=>$document)
-			<div class="col-md-3">
-				<h6> {!! $key !!}</h6>
-				<p>{!! $document !!}</p>
-			</div>
-		@endforeach
- 			</div>
-
-
-
- 		</div>	
-
- 		<div class="row col-md-12" style="padding-top:30px;">
- 			<div class="tab-item" style="display: block;">
-                <div class="faq-wrapper">
-                	@foreach($working_process as $key=>$val)
-                		 <div class="faq-item">
-                        <div class="faq-title">
-                            <h5 class="title">{!! $key !!}</h5>
-                            <span class="right-icon"></span>
-                        </div>
-                        <div class="faq-content">
-                            <p>
-                               {!! $val !!}
-                            </p>
-                        </div>
-                    </div>
-                	@endforeach
-                   
-                </div>
+<section class="faq-section padding-top padding-bottom">
+<div class="contener">
+ 	<div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10">
+            <div class="section-header">
+                <!-- <span class="cate">You have questions</span> -->
+                <h2 class="title">
+                  Documents Required 
+                </h2>
+                <p class="mw-100">
+                    Only Scanned Copies are needed, Scanned copy of PAN Card of all directors and Aadhar card/ Voter ID/ Passport/ Driving
+                </p>
             </div>
- 		</div>
- 	</div>
+        </div>
+    </div>
+ 	<div class="row"> 
+			@foreach($required_documents as $key=>$document)
+				<div class="col-md-3">
+					<h6> {!! $key !!}</h6>
+					<p>{!! $document !!}</p>
+				</div>
+			@endforeach
+	</div>	
+</div>
+</section>
+
+<section class="faq-section padding-top padding-bottom">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10">
+            <div class="section-header">
+                <!-- <span class="cate">You have questions</span> -->
+                <h2 class="title">
+                    Working Process
+                </h2>
+                <p class="mw-100">
+                    Working process step by step define bellow
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="faq-wrapper">
+    	<?php  $i=0; ?>
+    	@foreach($working_process as $key=>$val)
+    	<?php  $i++  ?>
+        <div class="faq-item <?= ($i == 1 ? 'open' : '' ) ?> ">
+            <div class="faq-title">
+                <h5 class="title">{!! $key !!}</h5>
+                <span class="right-icon"></span>
+            </div>
+            <div class="faq-content" style="display: <?= ($i ==1 ? 'block' : 'none' ) ?> ;">
+                <p>
+                    {!! $val !!}
+                </p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+ </section>		
+
 <style type="text/css">	
 	.how-item a.custom-button{
 	    color: white !important;
