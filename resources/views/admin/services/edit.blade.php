d076eaf7b2403082af96e4a7f2b435284837108b
Thakur Prasad <thakurprasadlhs@gmail.com>
2022-12-21 17:27:22 +0530

    ckediter use in service edit

--- resources/views/admin/services/edit.blade.php
+++ resources/views/admin/services/edit.blade.php
@@ -15,19 +15,23 @@
 		   	
    	<div class="container card">
    		<br><h5>Result Data</h5><br>
-   		<div class="col-lg-6" id="input_area">
+   		<div class="col-lg-6">
+   			<table class="table table-bordered" id="table_result_data">   			
    			<?php  $i = 0; ?>
 	   		@foreach($result_data as $key=>$val)
 	   		<?php $i++; ?>
-	          <div class="form-group"  id="row_no_<?= $i ?>">            
-	             <input class="form-control data-input" type="text" name="result_data[]" value="<?= $val ?>"><span class="delete_row" onclick="remove_row('row_no_{{$i}}')">x</span>
-	          </div>
+   				<tr  id="row_no_<?= $i ?>">
+   					<td style="width:80%">
+   						<input class="form-control" type="text" name="result_data[]" value="<?= $val ?>">	
+   					</td>
+   					<td style="width:20%">
+   						<span class="delete_row" onclick="remove_row('row_no_{{$i}}')">x</span>
+   					</td>
+   				</tr>   				
 	          @endforeach
-	       </div>
-	       <div class="col-lg-3">
-	       	
-	       </div>
-	        <a style="cursor: pointer;padding-right: 30%;"  onclick="new_row()">Add more</a>
+   			</table>
+	       </div>	       
+	        <a style="cursor: pointer;padding-right: 30%;"  onclick="add_more_result_data()">Add more</a>
 	    </div> 	
 		   </div>
 		    <div class="col-md-4">
@@ -40,11 +44,11 @@
 
 	    <div class="container card">
    		<br><h5>Required Documents</h5><br>
-	   		<table class="table table-bordered">
+	   		<table class="table table-bordered" id="table_required_documents">
 	   			<thead>
 	   				<tr>
-	   					<th class="w-20">Required Documents</th>
-	   					<th class="w-70">Required Documents Description</th>	
+	   					<th class="w-30">Required Documents</th>
+	   					<th class="w-60">Required Documents Description</th>	
 	   					<th class="w-10"></th>
 	   				</tr>
 	   			</thead>
@@ -57,7 +61,7 @@
 	   						<input type="text" name="required_documents[{{$j}}][document]" value="{{ $row->document }}" class="form-control">          
 	   					</td>
 	   					<td>
-	   						<input class="form-control data-input" type="text" name="required_documents[{{$j}}][document_desc]" value="<?= $row->document_desc ?>">	
+	   						<textarea id="rd_ckeditor_{{ $j }}" class="form-control data-input" type="text" name="required_documents[{{$j}}][document_desc]"><?= $row->document_desc ?></textarea>
 	   					</td>
 	   					<td>
 	   						<span class="delete_row" onclick="remove_table_row('table_row_{{$j}}')">x</span>
@@ -71,11 +75,11 @@
 
 	     <div class="container card">
    		<br><h5>Working Process</h5><br>
-	   		<table class="table table-bordered">
+	   		<table class="table table-bordered" id="table_working_process">
 	   			<thead>
 	   				<tr>
-	   					<th class="w-20">Title</th>
-	   					<th class="w-70">Description</th>	
+	   					<th class="w-30">Title</th>
+	   					<th class="w-60">Description</th>	
 	   					<th class="w-10"></th>
 	   				</tr>
 	   			</thead>
@@ -88,7 +92,7 @@
 	   						<input type="text" name="working_process[{{$k}}][title]" value="{{ $row->title }}" class="form-control" placeholder="Enter Title">          
 	   					</td>
 	   					<td>
-	   						<textarea class="form-control data-input" type="text" name="working_process[{{$k}}][description]"><?= $row->description ?></textarea>	
+	   						<textarea id="wp_ckeditor_{{ $k }}" class="form-control data-input" type="text" name="working_process[{{$k}}][description]"><?= $row->description ?></textarea>	
 	   					</td>
 	   					<td>
 	   						<span class="delete_row" onclick="remove_table_row('_table_row_{{$k}}')">x</span>
@@ -102,11 +106,11 @@
 
 	    <div class="container card">
    		<br><h5>Update Service Full Details</h5><br>
-	   		<table class="table table-bordered">
+	   		<table class="table table-bordered" id="table_details">
 	   			<thead>
 	   				<tr>
-	   					<th class="w-20">Title</th>
-	   					<th class="w-70">Description</th>	
+	   					<th class="w-30">Title</th>
+	   					<th class="w-60">Description</th>	
 	   					<th class="w-10"></th>
 	   				</tr>
 	   			</thead>
@@ -119,7 +123,7 @@
 	   						<input type="text" name="details[{{$m}}][title]" value="{{ $row->title }}" class="form-control" placeholder="Enter Title">          
 	   					</td>
 	   					<td>
-	   						<textarea id="ckeditor_{{ $m }}" class="form-control data-input" type="text" name="details[{{$m}}][description]"><?= $row->description ?></textarea>	
+	   						<textarea id="d_ckeditor_{{ $m }}" class="form-control data-input" type="text" name="details[{{$m}}][description]"><?= $row->description ?></textarea>	
 	   					</td>
 	   					<td>
 	   						<span class="delete_row" onclick="remove_table_row('1_table_row_{{$m}}')">x</span>
@@ -133,29 +137,38 @@
 	      
 	       <div class="col-lg-12">
 	       	<div class="col-md-3">
-		       	<input type="submit" name="update" value="Update" class="custom-button">
+		       	<input type="submit" name="update" value="Update" class="custom-button"  style="position: fixed;
+    right: 0;
+    top: 90%;
+    width: 200px;
+    z-index: 999;">
 		       </div>
 	       </div>
 
 
 
 	{!! Form::close() !!}
-<?php /*
-<textarea name="ckeditor_1"></textarea>
-<textarea name="ckeditor_2"></textarea>
+
+
+<!-- <textarea name="ckeditor_1"></textarea>
+<textarea name="ckeditor_2"></textarea> -->
 <script src="https://cdn.ckeditor.com/4.9.2/standard/ckeditor.js"></script>  
+<!-- <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>   -->
 <!-- https://www.c-sharpcorner.com/blogs/rich-textbox-editor-htmljavascript -->
 <script>  
+	/*
     CKEDITOR.replace('ckeditor_1');  
     CKEDITOR.replace('ckeditor_2');  
   
     function getData() {  
+    	alert("245");
         //Get data written in first Editor   
         var editor_data = CKEDITOR.instances['ckeditor_1'].getData();  
         //Set data in Second Editor which is written in first Editor  
         CKEDITOR.instances['ckeditor_2'].setData(editor_data);  
-    }  
-</script> */ ?> 	
+        alert("250");
+    }  */
+</script>
 <style type="text/css">
 .container.card {
     margin-bottom: 20px;
@@ -163,11 +176,11 @@
 .w-5{
 	width: 5%;
 }	
-.w-20{
-	width: 20%;
+.w-30{
+	width: 30%;
 }	
-.w-70{
-	width: 70%;
+.w-60{
+	width: 60%;
 }	
 
 .data-input {
@@ -182,59 +195,116 @@
 }
 </style>
 <script type="text/javascript">
-	var i =  {{ $i }};
-	function new_row(){		
-		i++;
-		var d = 'row_no_'+i;
-	var input = '<div id="row_no_'+i+'" class="form-group"><input type="text" class="form-control data-input" name="result_data[]"><span class="delete_row" onclick="remove_row(\''+d+'\')">x</span></div>';
-		document.getElementById('input_area').innerHTML += input;
-		
-	}
-
-	function remove_row(row_id){
-		
+	function remove_row(row_id){		
 		document.getElementById(row_id).innerHTML = '';
 	}
 
 
+	var i =  {{ $i }};
+	function add_more_result_data(){		
+		i++;
+		var d = 'row_no_'+i;
+		/*
+	var input = '<div id="row_no_'+i+'" class="form-group"><input type="text" class="form-control data-input" name="result_data[]"><span class="delete_row" onclick="remove_row(\''+d+'\')">x</span></div>';
+		document.getElementById('input_area').innerHTML += input;
+*/
+	var table = document.getElementById("table_result_data");
+	  var row = table.insertRow();
+	  row.setAttribute('id', d);
+	  var cell1 = row.insertCell(0);
+	  var cell2 = row.insertCell(1);
+	  cell1.innerHTML = '<input placeholder="Enter Result Data" type="text" name="result_data[]" class="form-control">';
+	  cell2.innerHTML = '<span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span>';
+		
+	}
+
+
+
 	var j =  {{ $j }};
+	<?php for ($i=1; $i <= $j ; $i++) { ?>
+	var c = 'rd_ckeditor_' + {{ $i }};
+	CKEDITOR.replace(c);
+	<?php } ?>
 	function add_more_required_documents(){		
 		j++;
 		var d = 'row_no_'+j;
-
+/*
 		var tr = '<tr id="'+d+'"><td><input type="text" name="required_documents['+j+'][document]" class="form-control"></td><td><input class="form-control data-input" type="text" name="required_documents['+j+'][document_desc]"></td><td><span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span></td></tr>';		
 
 		document.getElementById('table_body').innerHTML += tr;
+		*/
+
+	  var table = document.getElementById("table_required_documents");
+	  var row = table.insertRow();
+	  row.setAttribute('id', d);
+	  var cell1 = row.insertCell(0);
+	  var cell2 = row.insertCell(1);
+	  var cell3 = row.insertCell(2);
+	  cell1.innerHTML = '<input placeholder="Enter Title" type="text" name="required_documents['+j+'][document]" class="form-control">';
+	  cell2.innerHTML = '<textarea id="rd_ckeditor_'+j+'" class="form-control data-input"  placeholder="Enter Description" type="text" name="required_documents['+j+'][document_desc]"></textarea>';
+	  cell3.innerHTML = '<span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span>';
+	  CKEDITOR.replace('rd_ckeditor_'+j);  
 		
 	}
 
 
-var k =  {{ $k }};
+	var k =  {{ $k }};
+	<?php for ($i=1; $i <= $k ; $i++) { ?>
+	var c = 'wp_ckeditor_' + {{ $i }};
+	CKEDITOR.replace(c);
+	<?php } ?>
 	function add_more_working_process(){		
 		k++;
 		var d = 'row_no_'+k;
-
+/*
 		var tr = '<tr id="'+d+'"><td><input placeholder="Enter Title" type="text" name="working_process['+k+'][title]" class="form-control"></td><td><textarea class="form-control data-input"  placeholder="Enter Description" type="text" name="working_process['+k+'][description]"></textarea></td><td><span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span></td></tr>';	
-		document.getElementById('table_body_working_process').innerHTML += tr;
+		document.getElementById('table_body_working_process').innerHTML += tr;*/
 		
+
+	  var table = document.getElementById("table_working_process");
+	  var row = table.insertRow();
+	  row.setAttribute('id', d);
+	  var cell1 = row.insertCell(0);
+	  var cell2 = row.insertCell(1);
+	  var cell3 = row.insertCell(2);
+	  cell1.innerHTML = '<input placeholder="Enter Title" type="text" name="working_process['+k+'][title]" class="form-control">';
+	  cell2.innerHTML = '<textarea id="wp_ckeditor_'+k+'" class="form-control data-input"  placeholder="Enter Description" type="text" name="working_process['+k+'][description]"></textarea>';
+	  cell3.innerHTML = '<span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span>';
+	  CKEDITOR.replace('ckeditor_'+k);  
+
 	}
+
 
 var m =  {{ $m }};
-	function add_more_details(){		
+<?php for ($i=1; $i <= $m ; $i++) { ?>
+	var c = 'd_ckeditor_' + {{ $i }};
+	CKEDITOR.replace(c);
+<?php } ?>
+	  
+
+	function add_more_details(){
 		m++;
-		var d = 'row_no_'+m;
+	  var d = '1_table_row_'+m;
 
-		var tr = '<tr id="'+d+'"><td><input placeholder="Enter Title" type="text" name="details['+m+'][title]" class="form-control"></td><td><textarea class="form-control data-input"  placeholder="Enter Description" type="text" name="details['+m+'][description]"></textarea></td><td><span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span></td></tr>';	
-		document.getElementById('table_body_details').innerHTML += tr;
-		
+	  var table = document.getElementById("table_details");
+	  var row = table.insertRow();
+	  row.setAttribute('id', d);
+	  var cell1 = row.insertCell(0);
+	  var cell2 = row.insertCell(1);
+	  var cell3 = row.insertCell(2);
+	  cell1.innerHTML = '<input placeholder="Enter Title" type="text" name="details['+m+'][title]" class="form-control">';
+	  cell2.innerHTML = '<textarea id="d_ckeditor_'+m+'" class="form-control data-input"  placeholder="Enter Description" type="text" name="details['+m+'][description]"></textarea>';
+	  cell3.innerHTML = '<span class="delete_row" onclick="remove_table_row(\''+d+'\')">x</span>';
+	  CKEDITOR.replace('d_ckeditor_'+m);  
 	}
-
 
 
 	function remove_table_row(table_row){
 		   document.getElementById(table_row).remove();
 	}
 </script>
+
+
 
 </div>
 @endsection
